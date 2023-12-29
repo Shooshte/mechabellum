3bc7d50d47032b6005bbcc63a98f5e0a5c0b4587
shooshte <miha.sustersic.work@gmail.com>
2023-12-29 12:55:33 +0100

    MVP for testing

---
+++ lib/squadSuggestions.ts
@@ -0,0 +1,118 @@
+import squadsData from "../fixtures/squads.json";
+import type { Squad } from "../types/Squad";
+
+interface Args {
+  opponentArmy: Squad[];
+  yourArmy: Squad[];
+}
+
+export const getSquadSuggestions = ({
+  opponentArmy,
+  yourArmy,
+}: Args): string[] => {
+  // For every unit in opponents army
+  const unitsToCounter = opponentArmy
+    .map((currentSquad) => {
+      const opponentSquadData = squadsData.squads.find(
+        (squad) => currentSquad.name === squad.text
+      );
+      if (!opponentSquadData) {
+        throw new Error(`${currentSquad.name} data not found!`);
+      }
+
+      // Get list of your army units it is countered by
+      const fieldedCounters = yourArmy.reduce((acc: string[], yourSquad) => {
+        const isCounter = opponentSquadData.counteredBy?.some(
+          (squadName) => squadName === yourSquad.name
+        );
+
+        if (isCounter) {
+          return [...acc, yourSquad.name];
+        }
+
+        return acc;
+      }, []);
+
+      return {
+        ...currentSquad,
+        fieldedCounters,
+        tier: opponentSquadData.tier || 0,
+        counters: opponentSquadData.counters,
+        counteredBy: opponentSquadData.counteredBy,
+      };
+    })
+    // Sort opponent army by number of counters the units have in your army (less is better), resolve tie by tier
+    .sort((squadA, squadB) => {
+      const squadACountersCount = squadA.fieldedCounters.length;
+      const squadBCountersCount = squadB.fieldedCounters.length;
+
+      if (squadACountersCount === squadBCountersCount) {
+        return squadB.tier - squadA.tier;
+      }
+
+      return squadBCountersCount - squadACountersCount;
+    });
+
+  // For the first three units with the least counters
+  const possibleSuggestions = unitsToCounter
+    .slice(0, 3)
+    // Find units currently not on the field that counter them to get list of possible suggestions
+    .reduce((counters: string[], squadToCounter) => {
+      const remainingCounters =
+        squadToCounter.counteredBy
+          ?.filter((squad) => !squadToCounter.fieldedCounters.includes(squad))
+          .filter((squad) => !counters.includes(squad)) || [];
+
+      return [...counters, ...remainingCounters];
+    }, []);
+
+  // For list of possible suggestions
+  const sortedSuggestions = possibleSuggestions.sort(
+    (suggestionA, suggestionB) => {
+      // Sort by number of counters in opponents army (less is better), resolve tie by tier
+      const suggestionAData = squadsData.squads.find(
+        (squad) => squad.text === suggestionA
+      );
+      const suggestionBData = squadsData.squads.find(
+        (squad) => squad.text === suggestionB
+      );
+
+      const suggestionACounters = opponentArmy.reduce(
+        (acc: string[], oppSquad) => {
+          const isCountered = suggestionAData?.counteredBy?.some(
+            (squadName) => squadName === oppSquad.name
+          );
+
+          if (isCountered) {
+            return [...acc, oppSquad.name];
+          }
+
+          return acc;
+        },
+        []
+      );
+
+      const suggestionBCounters = opponentArmy.reduce(
+        (acc: string[], oppSquad) => {
+          const isCountered = suggestionBData?.counteredBy?.some(
+            (squadName) => squadName === oppSquad.name
+          );
+
+          if (isCountered) {
+            return [...acc, oppSquad.name];
+          }
+
+          return acc;
+        },
+        []
+      );
+
+      return suggestionBCounters.length - suggestionACounters.length;
+    }
+  );
+  // Return unit suggestions
+  return sortedSuggestions;
+
+  // TODO
+  // Add squad price and count to equation
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,59 @@
 "use client";
 
-import SquadComponent from "../components/Squad";
+import { useEffect, useMemo, useState } from "react";
+import { getSquadSuggestions } from "../lib/squadSuggestions";
 
+import SquadComponent from "../components/Squad";
 import styles from "../styles/Home.module.css";
 
 import type { Squad } from "../types/Squad";
 
 export default function Home() {
-  const onAdd = (squads: Squad[]) => {};
+  const [opponentArmy, setOpponentArmy] = useState<Squad[]>([]);
+  const [yourArmy, setYourArmy] = useState<Squad[]>([]);
+  const [squadSuggestions, setSquadSuggestions] = useState<string[]>([]);
+
+  const onOpponentArmyChange = (army: Squad[]) => {
+    setOpponentArmy(army);
+  };
+
+  const onYourArmyChange = (army: Squad[]) => {
+    setYourArmy(army);
+  };
+
+  useEffect(() => {
+    const suggestions = getSquadSuggestions({ opponentArmy, yourArmy });
+    const newSuggestions = suggestions.slice(0, 3);
+
+    console.log("suggestions: ", suggestions);
+    console.log("newSuggestions: ", newSuggestions);
+
+    setSquadSuggestions(newSuggestions);
+  }, [opponentArmy, yourArmy]);
+
+  const showSuggestions = useMemo(() => {
+    return squadSuggestions.length > 0;
+  }, [squadSuggestions]);
 
   return (
     <main className={styles.container}>
       <>
-        <SquadComponent onChange={onAdd} squadName="Opponent's Army" />
-        <SquadComponent onChange={onAdd} squadName="Your Army" />
+        <SquadComponent
+          onChange={onOpponentArmyChange}
+          squadName="Opponent's Army"
+        />
+        <SquadComponent onChange={onYourArmyChange} squadName="Your Army" />
         <section className={styles.section}>
           <h1 className={styles.sectionHeading}>Squad Suggestions</h1>
-          <p className={styles.notification}>WIP</p>
+          {showSuggestions ? (
+            <ul>
+              {squadSuggestions.map((squadSuggestion) => (
+                <li key={squadSuggestion}>{squadSuggestion}</li>
+              ))}
+            </ul>
+          ) : (
+            <p className={styles.notification}>No suggestions to show</p>
+          )}
         </section>
       </>
     </main>

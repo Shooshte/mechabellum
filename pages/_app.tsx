9a8a0a7e0e7a49db21030a62ea5736142334dda6
shooshte <miha.sustersic.work@gmail.com>
2024-01-05 11:41:07 +0100

    updated app icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,13 +49,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
